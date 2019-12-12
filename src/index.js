763a8ad676204f44b3bc0f9fb97d15a12caf1163
Hitesh Kumar <hitesh@singsys.com>
2019-12-12 17:10:01 +0530

    fixed recorder issue

--- src/index.js
+++ src/index.js
@@ -22,10 +22,12 @@ class VideoRecorder {
 	start() {
 		this.recording = document.createElement("video");		
 		this.preview.removeAttribute('controls')
-		this.preview.play()
+		this.data = [];
 		navigator.mediaDevices.getUserMedia(this.mediaSetting)
 			.then(stream => {
+				this.preview.src = null;
 				this.preview.srcObject = stream;
+				this.preview.play()
 				this.preview.captureStream = this.preview.captureStream || this.preview.mozCaptureStream;
 				return new Promise(resolve => this.preview.onplaying = resolve);
 			}).then(() => this._startRecording())
@@ -45,6 +47,9 @@ class VideoRecorder {
 	getBlob() {
 		return this._blob();
 	}
+	getRawData() {
+		return this.data;
+	}
 
 	_startRecording() {
 		this.recorder = new MediaRecorder(this.preview.captureStream());
@@ -60,7 +65,6 @@ class VideoRecorder {
 	_pauseRecorder() {
 		this.recorder.pause()
 	}
-
 	_resume() {
 		if (this.recorder) {
 			this.preview.removeAttribute('controls')
@@ -76,9 +80,10 @@ class VideoRecorder {
 		this.recording.src = URL.createObjectURL(recordedBlob);
 	}
 	_download(fileName) {
-		if (!this.recorder) {
+		if (!this.recorder && !this.data && !this.data.length) {
 			return;
 		}
+		
 		return this._blob()
 			.then(recordedBlob => {
 				let downloadButton = document.createElement("a");
@@ -91,6 +96,11 @@ class VideoRecorder {
 			})
 	}
 	_blob() {
+		if (!this.recorded && this.data && this.data.length) {
+			return new Blob(this.data, {
+				type: "video/webm"
+			});
+		}
 		return this._stop()
 			.then(data => {
 				return new Blob(data, {
@@ -99,6 +109,9 @@ class VideoRecorder {
 			})
 	}
 	_stop() {
+
+		if (!this.recorder) return
+
 		let stopped = new Promise((resolve, reject) => {
 			this.recorder.onstop = resolve;
 			this.recorder.onerror = event => reject(event.name);
@@ -109,8 +122,7 @@ class VideoRecorder {
 				recorded
 			]).then(() => this.data)
 			.then((data) => {
-				this.recorder = null;
-				this.data = [];
+				this.recorder = null;				
 				this.preview.srcObject.getTracks().forEach(track => track.stop());
 				this.preview.srcObject = null;
 				this.preview.src = this.recording.src
